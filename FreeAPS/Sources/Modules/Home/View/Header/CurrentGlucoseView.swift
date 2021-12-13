11704c7c360dfe5f9382787565af9ecc468c5832
bjornoleh <bjorn.haugsgjerd@gmail.com>
2021-12-13 23:38:49 +0100

    Add space before +/- delta

--- FreeAPS/Sources/Modules/Home/View/Header/CurrentGlucoseView.swift
+++ FreeAPS/Sources/Modules/Home/View/Header/CurrentGlucoseView.swift
@@ -22,7 +22,8 @@ struct CurrentGlucoseView: View {
         let formatter = NumberFormatter()
         formatter.numberStyle = .decimal
         formatter.maximumFractionDigits = 1
-        formatter.positivePrefix = "+"
+        formatter.positivePrefix = "  +"
+        formatter.negativePrefix = "  -"
         return formatter
     }
 
