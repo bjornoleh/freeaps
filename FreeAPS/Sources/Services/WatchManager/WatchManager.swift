7cf4e617efb963a6b1570927b3b7e5db71416c59
bjornoleh <bjorn.haugsgjerd@gmail.com>
2021-12-17 17:57:39 +0100

    WatchManager: Add to deltaFormatter: formatter.minimumFractionDigits = 1

--- FreeAPS/Sources/Services/WatchManager/WatchManager.swift
+++ FreeAPS/Sources/Services/WatchManager/WatchManager.swift
@@ -173,6 +173,7 @@ final class BaseWatchManager: NSObject, WatchManager, Injectable {
     private var deltaFormatter: NumberFormatter {
         let formatter = NumberFormatter()
         formatter.numberStyle = .decimal
+        formatter.minimumFractionDigits = 1
         formatter.maximumFractionDigits = 1
         formatter.positivePrefix = "+"
         return formatter
