9f71f043ef152ddfa963f3b68a9d43dc13fac756
bjornoleh <bjorn.haugsgjerd@gmail.com>
2021-12-23 00:15:22 +0100

    IOB: add formatter.minimumFractionDigits = 2

--- FreeAPS/Sources/Modules/Home/View/HomeRootView.swift
+++ FreeAPS/Sources/Modules/Home/View/HomeRootView.swift
@@ -14,6 +14,7 @@ extension Home {
             let formatter = NumberFormatter()
             formatter.numberStyle = .decimal
             formatter.maximumFractionDigits = 2
+            formatter.minimumFractionDigits = 2
             return formatter
         }
 
